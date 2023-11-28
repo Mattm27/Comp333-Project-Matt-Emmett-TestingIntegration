258b9577188b47bdf0f86d77caec90323ed3bdf0
Mattm27 <msmay@wesleyan.edu>
2023-11-28 14:37:02 -0500

    Small cleanup made to pytest testing

--- Automation/test_manipulate.py
+++ Automation/test_manipulate.py
@@ -11,7 +11,8 @@ import pytest
 
 @pytest.mark.int
 def test_integer_manipulator(input_int, expected_int):
-    assert integer_manipulator(input_int) == expected_int
+    actual = integer_manipulator(input_int)
+    assert integer_manipulator(input_int) == expected_int, (f"For {input_int}: expected {expected_int}, but got {actual}")
 
 @pytest.mark.parametrize("input_list, expected_list", [
     ([10, 2, 3, 0, "three"], [66, 2, 6, 0, 1]),
@@ -20,4 +21,5 @@ def test_integer_manipulator(input_int, expected_int):
 @pytest.mark.list
 def test_manipulate_list(input_list, expected_list):
     result = manipulate_list(input_list)
-    assert result == expected_list
+    for i, (actual, expected) in enumerate(zip(result, expected_list)):
+        assert actual == expected, (f"For input_list[{i}]: expected {expected}, but got {actual}")
